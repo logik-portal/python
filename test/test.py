d16b6a01cece43ccea8438614396eff29b080d5b
logik-portal <85372329+logik-portal@users.noreply.github.com>
2026-02-26 11:51:08 -0500

    test

--- test/test.py
+++ test/test.py
@@ -1,4 +1,4 @@
-# SRT to XML
+# Test
 # Copyright (c) 2025 Michael Vaglienty
 #
 # This program is free software: you can redistribute it and/or modify
@@ -7,15 +7,6 @@
 # (at your option) any later version.
 #
 # This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program. If not, see <https://www.gnu.org/licenses/>.
-#
-# License:       GNU General Public License v3.0 (GPL-3.0)
-#                https://www.gnu.org/licenses/gpl-3.0.en.html
 
 """
 Script Name: Test
