60f264c706b053ca26ed211488f044fdc78c788d
logik-portal <85372329+logik-portal@users.noreply.github.com>
2026-02-26 14:26:00 -0500

    Update test.py

--- test/test.py
+++ test/test.py
@@ -11,7 +11,7 @@
 
 """
 Script Name: Test
-Script Version: 3.7.7
+Script Version: 3.7.8
 Flame Version: 2023.2
 Written by: Michael Vaglienty
 Creation Date: 05.01.20
@@ -38,8 +38,8 @@ To install:
 
 Updates:
 
-    v3.7.7 02.26.26
-        - Updated to PyFlameLib v7.7.7.
+    v3.7.8 02.26.26
+        - Updated to PyFlameLib v7.7.62.
 
     v3.7.5 02.26.26
         - Updated to PyFlameLib v6.6.6.
