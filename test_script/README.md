5959bfede3f1b22b5df8d833608837936962c898
logik-portal <85372329+logik-portal@users.noreply.github.com>
2026-09-10 12:59:31 -0400

    Initial Commit

---
+++ test_script/README.md
@@ -0,0 +1,26 @@
+# Test Script
+
+**Script Version:** 1.0.0  
+**Flame Version:** 2027  
+**Written by:** Michael Vaglienty  
+**Creation Date:** 09.10.26  
+**Update Date:** 09.10.26  
+
+**Script Type:** Batch
+
+## Description
+
+This is a test. This does nothing.
+
+## Menus
+
+- None
+
+## Installation
+
+Copy script into /opt/Autodesk/shared/python/test_script
+
+## Updates
+
+### v1.0.0 [09.10.26]
+- Blah
---
+++ test_script/test_script.py
@@ -0,0 +1,45 @@
+"""
+Script Name: Test Script
+Script Version: 1.0.0
+Flame Version: 2027
+Written by: Michael Vaglienty
+Creation Date: 09.10.26
+Update Date: 09.10.26
+
+License: GNU General Public License v3.0 (GPL-3.0) - see LICENSE file for details
+
+Script Type: Batch
+
+Description:
+
+    This is a test. This does nothing.
+
+Menus:
+
+    None
+
+To install:
+
+    Copy script into /opt/Autodesk/shared/python/test_script
+
+Updates:
+
+    v1.0.0 09.10.26
+        - Blah
+"""
+
+import os
+
+# ==============================================================================
+# [Constants]
+# ==============================================================================
+
+SCRIPT_NAME = 'Test Script'
+SCRIPT_VERSION = 'v1.0.0'
+SCRIPT_PATH = os.path.abspath(os.path.dirname(__file__))
+
+# ==============================================================================
+# [Main Script]
+# ==============================================================================
+
+print('Hello, World!')
